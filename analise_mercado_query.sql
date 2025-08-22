d4e877d6b88c8c3d04099e48bf977440fe7b9dd6
PepoLab <rockpedro331@gmail.com>
2025-08-22 09:49:44 -0300

    Ajuste no insert dos salário

--- analise_mercado_query.sql
+++ analise_mercado_query.sql
@@ -15,19 +15,15 @@ CREATE TABLE salario_dados (
 insert into salario_dados (Cargo,Nivel,SalarioMin,SalarioMax,Localidade,FONTE)
     VALUES
 ('Analista de Dados', 'Pleno', 5500.00, 8000.00, 'Brasil', 'somostera.com'),
+('Analista de Dados', 'Júnior', 4000.00,  4800.00, 'Brasil', 'estimativa mercado'),
+('Analista de Dados', 'Sênior', 7500.00,  9000.00, 'Brasil', 'estimativa mercado'),
 ('Cientista de Dados', 'Júnior', 8588.80, 10306.56, 'Brasil', 'salario.com.br'),
 ('Cientista de Dados', 'Pleno', 11497.79, 13797.35, 'Brasil', 'salario.com.br'),
 ('Cientista de Dados', 'Sênior', 14878.73, 20000.00, 'Brasil', 'salario.com.br'),
-('Engenheiro de Dados', 'Júnior', 13748.38, 16498.06, 'São Paulo (Estado)', 'salario.com.br'),
-('Engenheiro de Dados', 'Júnior', 13748.38, 16498.06, 'São Paulo (Estado)', 'salario.com.br'),
-('Engenheiro de Dados', 'Sênior', 23709.52, 28451.42, 'São Paulo (Estado)', 'salario.com.br'),
 ('Engenheiro de Dados', 'Júnior', 15726.80, 18872.16, 'São Paulo (Capital)', 'salario.com.br'),
 ('Engenheiro de Dados', 'Pleno', 20996.73, 25196.08, 'São Paulo (Capital)', 'salario.com.br'),
 ('Engenheiro de Dados', 'Sênior', 27112.04, 32534.45, 'São Paulo (Capital)', 'salario.com.br');
 
---- Incremento de valores 
-insert into salario_dados (Cargo,Nivel,SalarioMin,SalarioMax,Localidade,FONTE)
-    VALUES
-('Analista de Dados', 'Júnior', 4000.00,  4800.00, 'Brasil', 'estimativa mercado'),
-('Analista de Dados', 'Sênior', 7500.00,  9000.00, 'Brasil', 'estimativa mercado')
+
+
 
